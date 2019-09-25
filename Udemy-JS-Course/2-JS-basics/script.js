60429c55d2db5023f1cfd592d2e16a9cf9eacf7a
Ashley Arrue <ashleydarrue@gmail.com>
2019-09-25 19:45:41 -0400

    new video started

--- Udemy-JS-Course/2-JS-basics/script.js
+++ Udemy-JS-Course/2-JS-basics/script.js
@@ -203,3 +203,12 @@ var BMIJohn = massJohn / (heightJohn * heightJohn);
   console.log(firstName + ' is a Man.');
  }
  */
+
+ /*****************************
+  * The Ternary Operator and Switch Statements
+  */
+ 
+  // var firstName = 'John';
+  // var age = 16;
+
+  // age >= 18 ? console.log(firstName + 'drinks beer.') : console.log(firstName + ' drinks juice.')
