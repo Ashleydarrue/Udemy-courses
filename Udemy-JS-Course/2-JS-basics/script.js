8974fbdf00ed40dd0fad80e83abb2cacc1ad0bf0
Ashley Arrue <ashleydarrue@gmail.com>
2019-10-09 14:40:04 -0400

    revising old lessons

--- Udemy-JS-Course/2-JS-basics/script.js
+++ Udemy-JS-Course/2-JS-basics/script.js
@@ -214,4 +214,21 @@ var BMIJohn = massJohn / (heightJohn * heightJohn);
 
 // age >= 18 ? console.log(firstName + 'drinks beer.') : console.log(firstName + ' drinks juice.')
 
-  
+  /**********************
+ * Boolean logic
+ */
+
+ /*
+ var firstName = 'John';
+ var age = 20;
+
+ if (age < 13){
+   console.log(firstName + ' is a boy.');
+ } else if (age >= 13 && age < 20) {// between 13 and 20
+  console.log(firstName + ' is a teenager.');
+ } else if (age >= 20 && age < 30){
+  console.log(firstName + ' is a young man.');
+ } else {
+  console.log(firstName + ' is a Man.');
+ }
+ */
