30758e2147dfca3b89d090913f02c271b8f43678
Ashley Arrue <ashleydarrue@gmail.com>
2019-10-10 14:41:11 -0400

    revising old lessons

--- Udemy-JS-Course/2-JS-basics/script.js
+++ Udemy-JS-Course/2-JS-basics/script.js
@@ -1,24 +1,3 @@
-/************************
- Variable and data types
-*/
-
-
-// var firstName = 'John';
-// console.log(firstName);
-
-// var lastName = "Smith";
-// var age = 28;
-
-// var fullAge = true;
-// console.log(fullAge);
-
-// var job;
-// console.log(job);
-
-// // Variable naming rules
-// var _3years = 3;
-// var johnMark = "Jpohn and Mark";
-// var if = 23;
 
 
 /************************
