a3fba1103e12266fee0815a01d75f7c9bd42cf9e
Ashley Arrue <ashleydarrue@gmail.com>
2019-11-21 12:51:24 -0500

    cleaned up the code

--- Traversy-Media/react_crash_todo/src/components/Todoitem.js
+++ Traversy-Media/react_crash_todo/src/components/Todoitem.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+
+
 export class Todoitem extends Component {
   getStyle= () => {
      return{
-       background: '#f4f4f4' ,
+       background: '#f4f4f4',
        padding: '10px',
        borderBottom: '1px #ccc dotted' ,
-       textDecoration: this.props.todo.completed ? 'line-through' : 'none' ,
-
+       textDecoration: this.props.todo.completed ? 'line-through' : 'none'
       }
     }
     
