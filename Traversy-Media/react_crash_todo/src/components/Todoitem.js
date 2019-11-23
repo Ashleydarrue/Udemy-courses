9c5c7f126e37a8bad9ae80fbb043adfb19a7742f
Ashley Arrue <ashleydarrue@gmail.com>
2019-11-23 00:09:15 -0500

    Todo List app Completed

--- Traversy-Media/react_crash_todo/src/components/Todoitem.js
+++ Traversy-Media/react_crash_todo/src/components/Todoitem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 
 export class Todoitem extends Component {
+
   getStyle= () => {
      return{
        background: '#f4f4f4',
@@ -12,25 +13,36 @@ export class Todoitem extends Component {
       }
     }
     
-    markComplete = (e) => {
-      console.log(this.props)
-    }
 
   render() {
+    const { id, title} = this.props.todo
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox"  onChange={this.markComplete} />{'  '}
-          {this.props.todo.title}
+          <input type="checkbox"  onChange={this.props.markComplete.bind(this, id)} />{'  '}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>X</button>
           </p>
       </div>
     )
   }
 }
 
+const btnStyle ={
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 9px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
+}
+
 // PropTypes
 Todoitem.propTypes ={
-  todo: PropTypes.object.isRequired
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 } 
 
 
--- Traversy-Media/react_crash_todo/src/components/Todos.js
+++ Traversy-Media/react_crash_todo/src/components/Todos.js
@@ -4,16 +4,22 @@ import PropTypes from 'prop-types';
 
 class Todos extends Component {
   render(){
-    console.log(this.props.todos);
     return this.props.todos.map((todo)=> (
-      <Todoitem key={todo.id}todo={todo}/>
+      <Todoitem 
+      key={todo.id}
+      todo={todo} 
+      markComplete={this.props.markComplete}  
+      delTodo={this.props.delTodo} 
+      />
     ));
     }
 }
 
 // PropTypes
 Todos.propTypes ={
-  todos: PropTypes.array.isRequired
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 }
 
 export default Todos;
