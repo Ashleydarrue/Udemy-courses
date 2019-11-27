eaf7006f5a134a3519a858144d9d14d779255a24
Ashley Arrue <ashleydarrue@gmail.com>
2019-11-27 12:53:46 -0500

    solved a challenge

--- Udemy-JS-Course/2-JS-basics/script.js
+++ Udemy-JS-Course/2-JS-basics/script.js
@@ -102,3 +102,31 @@ console.log(firstName + ' ' + lastName);
 
     // const inAscOrder = arr => arr.every((_, i) => i === 0 || arr[i] > arr[i - 1])
 */
+
+
+/*
+  Title:
+    Basic JS - Building a calculator
+  Description:
+    Let's build a calculator.
+    This is very basic Javascript kata.
+    The test expects you to provide a Calculator object with the following methods:
+    Calculator.add()
+    Calculator.subtract()
+    Calculator.multiply()
+    Calculator.divide()
+    Each method expects two arguments, so for instance:
+    Calculator.add(1,4) should return 5.
+    Only integers are expected to be passed in as arguments,
+    and the divide method should return 'false' when trying the divide by zero.
+
+    //SOLUTION
+      const Calculator = {
+  add: (x, y) => x + y,
+  subtract: (x, y) => x - y,
+  multiply: (x, y) => x * y,
+  divide: (x, y) => (y === 0 ? false : x / y),
+}
+
+
+    */
