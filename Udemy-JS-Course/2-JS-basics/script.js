edef247f1a7a4cabb8c497e5c550e2f64d34ddd6
Ashley Arrue <ashleydarrue@gmail.com>
2019-11-29 12:57:30 -0500

    solved a challenge

--- Udemy-JS-Course/2-JS-basics/script.js
+++ Udemy-JS-Course/2-JS-basics/script.js
@@ -152,3 +152,29 @@ while (count <= 10) {
   },
 }
     */
+
+    /*
+  Task: Basic Math (Add or Subtract)
+  Description:
+    In this kata, you will do addition and subtraction on a given string.
+    The return value must be also a string.
+  Examples:
+    "1plus2plus3plus4"  --> "10"
+    "1plus2plus3minus4" -->  "2"
+  Notes:
+    The input will not be empty.
+
+    //SOLUTION
+
+    const calculate = str =>
+  String(
+    str
+      .split('plus')
+      .join(' ')
+      .split('minus')
+      .join(' -')
+      .split(' ')
+      .reduce((total, num) => total + Number(num), 0)
+  )
+
+  */
