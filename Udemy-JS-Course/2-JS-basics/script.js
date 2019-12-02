0c4f2b9f6e866d612a36f28268537b88e9940e2a
Ashley Arrue <ashleydarrue@gmail.com>
2019-12-02 13:01:52 -0500

    solved a challenge

--- Udemy-JS-Course/2-JS-basics/script.js
+++ Udemy-JS-Course/2-JS-basics/script.js
@@ -178,3 +178,58 @@ while (count <= 10) {
   )
 
   */
+/*
+  Task: Check three and two
+  Description:
+    Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran),
+    check if the array contains three and two of the same values.
+  Examples:
+    ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+    ["a", "b", "c", "b", "c"] ==> false // 1x "a", 
+
+    //SOLUTION
+    const totalLetters = (array, string) =>
+  array.filter(element => element === string).length
+
+const checkThreeAndTwo = array => {
+  const totalA = totalLetters(array, 'a')
+  const totalB = totalLetters(array, 'b')
+  const totalC = totalLetters(array, 'c')
+
+  if (totalA === 3 && (totalB === 2 || totalC === 2)) return true
+  if (totalB === 3 && (totalA === 2 || totalC === 2)) return true
+  if (totalC === 3 && (totalA === 2 || totalB === 2)) return true
+
+  return false
+}
+
+*/
+
+/*
+  Task: Who likes it?
+  Description:
+    You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items.
+    We want to create the text that should be displayed next to such an item.
+    Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item.
+  Examples:
+    It must return the display text as shown in the examples:
+    likes [] // must be "no one likes this"
+    likes ["Peter"] // must be "Peter likes this"
+    likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+    likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+    likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+  Notes:
+    For more than 4 names, the number in and 2 others simply increases.
+
+    //SOLUTION
+    const likes = (names = []) => {
+  if (names.length === 0) return `no one likes this`
+  if (names.length === 1) return `${names[0]} likes this`
+  if (names.length === 2) return `${names[0]} and ${names[1]} like this`
+  if (names.length === 3)
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+
+  return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+}
+
+*/
