d7ce82396abc333e8bce19bf6dcc8d34a891d0b7
Ashley Arrue <ashleydarrue@gmail.com>
2019-12-09 11:45:58 -0500

    codewars solution'

--- Udemy-JS-Course/2-JS-basics/script.js
+++ Udemy-JS-Course/2-JS-basics/script.js
@@ -350,3 +350,21 @@ const getNaughtyNames = people =>
       .join('')
   )
     */
+
+
+/*
+  Task : Evens times last
+  Description:
+    Given an array of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
+    If the array is empty, you should return 0.
+
+    //SOLUTION
+     const evenLast = numbers => {
+  if (numbers.length === 0) return 0
+
+  const lastInteger = numbers[numbers.length - 1]
+  const sum = numbers.reduce((acc, number, index) => (index % 2 === 0 ? acc + number : acc), 0)
+
+  return sum * lastInteger
+}
+    */
