9107a79a582f99f579c4f0314eb1e44050d4e44f
Ashley Arrue <ashleydarrue@gmail.com>
2019-12-11 11:49:36 -0500

    codewars solution'

--- Udemy-JS-Course/2-JS-basics/script.js
+++ Udemy-JS-Course/2-JS-basics/script.js
@@ -382,3 +382,20 @@ const getNaughtyNames = people =>
     //SOLUTION
    const findDup = arr => arr.find(num => arr.indexOf(num) !== arr.lastIndexOf(num))
     */
+
+
+    /*
+  
+  Task: Find twins
+  Description:
+    Agent 47, you have a new task! Among citizens of the city X are hidden 2 dangerous criminal twins.
+    You task is to identify them and eliminate!
+    Given an array of integers, your task is to find two same numbers and return one of them, for example in array [2, 3, 6, 34, 7, 8, 2] answer is 2.
+    If there are no twins in the city - return None or the equivilent in the langauge that you are using.
+
+    //SOLUTION
+const elimination = arr =>
+  arr.find(number => arr.indexOf(number) !== arr.lastIndexOf(number)) || null
+
+
+    */
