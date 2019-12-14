3ea6f92ce9e5a0907c6da5b13558798f9c8db80f
Ashley Arrue <ashleydarrue@gmail.com>
2019-12-14 11:55:18 -0500

    codewars solution'

--- Udemy-JS-Course/2-JS-basics/script.js
+++ Udemy-JS-Course/2-JS-basics/script.js
@@ -435,3 +435,17 @@ const elimination = arr =>
     //SOLUTION
    const friend = friends => friends.filter(friend => friend.length === 4)
     */
+
+
+
+    /*
+  Title:
+    Array plus array
+  Description:
+    I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
+    I'll appreciate for your help.
+
+    //SOLUTION
+   const arrayPlusArray = (arr1, arr2) =>
+  [...arr1, ...arr2].reduce((total, value) => total + value, 0)
+    */
