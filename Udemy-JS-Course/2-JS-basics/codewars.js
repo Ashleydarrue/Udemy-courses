fdc881b1732b76d2d97ffe8ff3521bf791ddeb5a
Ashley Arrue <ashleydarrue@gmail.com>
2020-01-02 14:02:50 -0500

    new Codewars challenge

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -14,3 +14,30 @@
 // }
 
 // console.log(startsWithS(peanuts))
+
+/* Welcome Codewars
+
+function greet(language) {
+	const start ={
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+}
+  return start[language] || start.english;
+}
+
+*/
