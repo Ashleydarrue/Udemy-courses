a6033cecdfc01d1220699e2afaa96737675d9ae5
Ashley Arrue <ashleydarrue@gmail.com>
2020-01-06 14:37:18 -0500

    new Codewars challenge

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -74,3 +74,10 @@ function covfefe(str){
   return str+" covfefe"
 }
 */
+
+/* calculate average 
+
+function find_average(array) {
+  return array.reduce((result, current) => result + current, 0) / array.length;
+}
+*/
