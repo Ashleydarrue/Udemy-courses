5b5ce67547187d8c325844b66e6bd36b7207d53c
Ashley Arrue <ashleydarrue@gmail.com>
2020-01-07 14:58:41 -0500

    new Codewars challenge

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -96,3 +96,22 @@ function divide(weight){
  return false
 }
 */
+
+/* Whtas up next?
+const nextItem = (xs, item) => {
+  let found = false
+  let finalValue
+
+  for (const i of xs) {
+    if (found) {
+      finalValue = i
+      break
+    }
+
+    if (i === item) found = true
+  }
+
+  return finalValue
+}
+
+*/
