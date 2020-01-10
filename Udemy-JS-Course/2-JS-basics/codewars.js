a1cf3f196e1d3c47a4ffbfe1f1ba4e1ed49ce264
Ashley Arrue <ashleydarrue@gmail.com>
2020-01-10 09:39:24 -0500

    cleaned up the files a bit

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -390,3 +390,11 @@ function opposite(number) {
 }
 
 */
+
+// Grasshopper - Summation
+
+// function summation(num) {
+//   let result = 0
+//   for (let i = 1; i <= num; i++) result += i
+//   return result
+// }
