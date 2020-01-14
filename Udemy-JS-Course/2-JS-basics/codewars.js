fa8e37d5b1027e0f53a0094f4a61cacc03f3f335
Ashley Arrue <ashleydarrue@gmail.com>
2020-01-14 09:46:37 -0500

    cleaned up the files a bit

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -424,3 +424,10 @@ const plural = n => n !== 1
 /* My head is at the wrong end! 
 const fixTheMeerkat = array => array.reverse()
 */ 
+
+/*  Are You Playing Banjo?
+function areYouPlayingBanjo(name) {
+  if (name[0] === 'R' || name[0] === 'r') return `${name} plays banjo`
+  return `${name} does not play banjo`
+}
+*/
