79d7bd16237825aac81804b1648c100f1278983b
Ashley Arrue <ashleydarrue@gmail.com>
2020-01-15 09:46:33 -0500

    cleaned up the files a bit

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -431,3 +431,12 @@ function areYouPlayingBanjo(name) {
   return `${name} does not play banjo`
 }
 */
+
+/*
+Compare within margin
+const closeCompare = (a, b, margin = 0) => {
+  if (Math.abs(a - b) <= margin) return 0
+  if (a < b) return -1
+  return 1
+}
+*/
