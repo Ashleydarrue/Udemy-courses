d67ebe15da5c9e7177ef4ff263b4a9bfaad989f7
Ashley Arrue <ashleydarrue@gmail.com>
2020-01-18 09:51:06 -0500

    cleaned up the files a bit

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -444,3 +444,13 @@ const closeCompare = (a, b, margin = 0) => {
 /*  isReallyNaN 
 const isReallyNaN = value => Number.isNaN(value)
 */
+
+/* Grader
+function grader(score) {
+  if (score > 1 || score < 0.6) return 'F'
+  if (score >= 0.9) return 'A'
+  if (score >= 0.8) return 'B'
+  if (score >= 0.7) return 'C'
+  if (score >= 0.6) return 'D'
+}
+ */
