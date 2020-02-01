f42688259075e02adf6ac3e3649bd88089dc0882
Ashley Arrue <ashleydarrue@gmail.com>
2020-02-01 10:13:37 -0500

    cleaned up the files a bit

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -551,7 +551,46 @@ const validateWord = s =>
  const chain = (input, fs) => fs.reduce((total, func) => func(total), input)
  */
 
- /* */
+ /* adding words 
+ class Arith {
+  constructor(initialWordNumber) {
+    this.numberToWordDictionary = {
+      zero: 0,
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9,
+      ten: 10,
+      eleven: 11,
+      twelve: 12,
+      thirteen: 13,
+      fourteen: 14,
+      fifteen: 15,
+      sixteen: 16,
+      seventeen: 17,
+      eighteen: 18,
+      nineteen: 19,
+      twenty: 20,
+    }
+    this.wordToNumberDictionary = Object.keys(this.numberToWordDictionary)
+    this.initialNumber = this.getNumberFromWord(initialWordNumber)
+  }
+  getNumberFromWord(word) {
+    return this.numberToWordDictionary[word]
+  }
+  getWordFromNumber(number) {
+    return this.wordToNumberDictionary[number]
+  }
+  add(newNumber) {
+    const sum = this.initialNumber + this.getNumberFromWord(newNumber)
+    return this.getWordFromNumber(sum)
+  }
+}*/
 
  /* */
 
