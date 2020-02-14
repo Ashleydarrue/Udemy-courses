6f4503f0dab3208092d8cc3c2fb8e7f3f22a110b
Ashley Arrue <ashleydarrue@gmail.com>
2020-02-13 19:15:43 -0500

    new Codewars challenge

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -128,8 +128,15 @@ function babyRepeat(babble){
 }
 */
 
-/*
-
+/* All Inclusive?
+function containAllRots(str, arr) {
+  for (var i = 0; i < str.length; i++) {
+    if (arr.indexOf(str.slice(i) + str.slice(0, i)) === -1) {
+      return false
+    }
+  }
+  return true
+}
 */
 
 /*
