d7e9bb1c18948ec3de2b5950117d93acc565eebc
Ashley Arrue <ashleydarrue@gmail.com>
2020-02-18 19:17:13 -0500

    new Codewars challenge

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -139,7 +139,11 @@ function containAllRots(str, arr) {
 }
 */
 
-/*
+/* Alternate Square Sum
+function alternateSqSum(arr){
+  return arr.map((val, i) => i % 2 ? Math.pow(val, 2) : val)
+    .reduce((a, v) => a + v, 0);
+}
 
 */
 
