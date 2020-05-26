fcd517946a33b05cf94ffc3ebcc9784a86db5981
Ashley Arrue <ashleydarrue@gmail.com>
2020-05-25 23:47:16 -0400

    new codewar

---
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -0,0 +1,16 @@
+// find names that start with S
+
+// let peanuts = ["Sally", "Snoopy", "Char" , "lucy" , "Sam"]
+
+// function startsWithS(array) {
+//     var sArray=[]
+//    array.forEach(function(element) {
+//         if (element.startsWith('S')) {
+//             sArray.push(element)
+//         }
+//     });
+
+//     return sArray;
+// }
+
+// console.log(startsWithS(peanuts))
