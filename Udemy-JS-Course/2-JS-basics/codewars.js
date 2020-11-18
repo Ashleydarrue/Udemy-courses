a65734949e76654bf5c55f1ca0336edb5862a495
Ashley Arrue <ashleydarrue@gmail.com>
2020-11-18 09:24:03 -0500

    cleaned up the files a bit

--- Udemy-JS-Course/2-JS-basics/codewars.js
+++ Udemy-JS-Course/2-JS-basics/codewars.js
@@ -353,7 +353,16 @@ function findOdd(array) {
 
 */
 
-/*
+/* Consecutive strings
+function longestConsec(strarr, k) {
+  const n = strarr.length
+  if (n === 0 || k > n || k <= 0) return ''
+
+  return strarr.reduce((longest, item, i) => {
+    const combined = strarr.slice(i, i + k).join('')
+    return combined.length > longest.length ? combined : longest
+  }, '')
+}
 
 */
 
